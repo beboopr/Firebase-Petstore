6297ecee2832ce450993f828660936224fa7ad5b
beboopr <linnets-router.09@icloud.com>
2022-10-14 13:41:21 -0400

    firestore-project

--- index.js
+++ index.js
@@ -7,3 +7,50 @@ initializeApp({
 })
 
 const db = getFirestore();
+
+//1.Customers
+// const NewCustomer = {
+//     name: 'katya',
+//     phone: 7545640064,
+//     location: 'Deerfield beach',
+//     email: "Katyac8@gmail.com"
+// }
+
+// db.collection('Customer').add(NewCustomer)
+
+// .then(doc => console.log('Customer created', doc.id))
+// .catch(console.error)
+
+//2.Products
+// const NewPets = {
+//     name: '',
+//     species: 'Cat',
+//     breed: 'shit-zu',
+//     vacinated: false,
+//     age: '3years',
+// }
+
+// db.collection('Pets').add(NewPets)
+
+// .then(doc => console.log('Pet created', doc.id))
+// .catch(console.error)
+
+
+const NewProducts = {
+    name: 'Kong',
+    serialnumber: 359067,
+    datestock: 2015,
+    price: '$24.99',
+}
+
+db.collection('Product').add(NewProducts)
+
+.then(doc => console.log('Product created', doc.id))
+.catch(console.error)
+
+// .then(collection => {
+//     collection.docs.forEach(doc => {
+//         console.log(doc.id, doc.data())
+//     })
+// })
+// .catch(console.error)
